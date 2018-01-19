80c191bdafa114c19cd219fe8dd8549df92fabe2
dlarrington <35306140+dlarrington@users.noreply.github.com>
2018-01-19 00:03:18 -0600

    website update

---
+++ DS Level 01/Apples.py
@@ -0,0 +1,14 @@
+#Apples
+#Programmer: Dalton Larrington
+#Date: 1-18-18
+
+prices = [14, 6, 9, 7, 8, 10, 3, 9]
+     
+for i in range(len(prices)):
+
+     if i < 100:
+
+          print ("You can buy this many apples: ")
+    
+
+    
