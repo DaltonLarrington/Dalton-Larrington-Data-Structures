bc14e86b3a01a70ac46db32f8fe29603cd39fc28
Dalton Larrington <dalton2@graceland.edu>
2018-01-26 11:59:00 -0600

    website update

---
+++ doctorsoffice.py
@@ -0,0 +1,24 @@
+#The Doctor's Office
+#Programmer: Dalton Larrington
+#Date: 1-26-2017
+
+#Patient
+
+def patient():    
+
+#Waiting Room
+
+def waitingRoom():
+
+#Nurse
+
+def nurse():
+
+#Exam Room
+
+def examRoom():
+
+#Physican
+def physican():
+
+    
