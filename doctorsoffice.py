0f5387db94260b1c64a76f2d587b88879595294f
dlarrington <35306140+dlarrington@users.noreply.github.com>
2018-01-27 12:39:57 -0600

    website update

--- doctorsoffice.py
+++ doctorsoffice.py
@@ -2,23 +2,56 @@
 #Programmer: Dalton Larrington
 #Date: 1-26-2017
 
-#Patient
+# We know there needs to be 6 exam rooms
+# Each patient will need to be in the exam room for about 17.5 minutes
+# Each patient can enter the waiting room twice
+# There can be about 3.43 patients per exam room per hour
+# There can be about 24 patients per hour
+# Therefore there needs to be 6 doctors
 
-def patient():    
-
-#Waiting Room
+##Waiting Room
 
 def waitingRoom():
+    
+    keep track of number of patients in waitingRoom
+    
+##Patient
 
-#Nurse
+def patient():
+
+    pat = patient
+
+    set timer of each patient to a random between 15-20 minutes
+
+    then remove patient from simulation     
+
+##Nurse
 
 def nurse():
 
-#Exam Room
+    check for number of open examRoom
+    
+    if examRoom >= 1:
+
+        then send patient in waitingRoom to examRoom
+
+##Exam Room
 
 def examRoom():
 
-#Physican
+    rooms = 6
+
+    take 6 patients from waitingRoom    
+
+##Physican
+    
 def physican():
+
+    doctors = 6
+
+    subtract the number of doctors with the number of patients in examRoom
+    
+
+    
 
     
