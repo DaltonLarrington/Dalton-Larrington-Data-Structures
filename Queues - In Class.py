d701d13dc8d38459fa9c9783f1cfad414fcb0850
Dalton Larrington <dalton2@graceland.edu>
2018-03-07 12:44:07 -0600

    website update

---
+++ Queues - In Class.py
@@ -0,0 +1,22 @@
+
+class myQueue:
+
+    def __init__:
+
+        self.head = None
+        self.tail = None
+
+    def pop(self):
+
+        #Create a new variable rVal = self.head
+        #set self.head = tail.head
+        #return rVal
+
+        return self.head
+
+    def append(self, value):
+
+        pass
+        #add to tail of queue
+
+    
